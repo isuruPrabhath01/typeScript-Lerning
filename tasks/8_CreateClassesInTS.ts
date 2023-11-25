27826f79bd25c81149e2e9e0f239145d93313a88
isuruPrabhath01 <isuruprabhath111@gmail>
2023-11-25 15:35:45 +0530

    Create Classes

--- tasks/8_CreateClassesInTS.ts
+++ tasks/8_CreateClassesInTS.ts
@@ -2,12 +2,15 @@
 
 class Person{
     name : string;
-    age : number;
+    private age : number;
     readonly nic : string;
     constructor(name : string , age : number , nic : string) {
         this.name=name;
         this.age=age;
         this.nic=nic;
+    }
+    getAge(){
+        return this.age;
     }
 }
 
@@ -15,6 +18,6 @@ let person1 = new Person("Isuru", 22, "200123601322");
 console.log(person1);
 person1.name="Prabhath";
 console.log(person1);
-person1.age=21;
-console.log(person1);
+//console.log(person1.age):// cannot access out of class scope
+console.log(person1.getAge()); // access using get method
 //person1.nic="200123601323";  //Cannot update because "readonly" keyword;
