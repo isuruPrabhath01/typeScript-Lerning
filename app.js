40916c632e9f3c11138c6ad05c4b5db7df19287f
isuruPrabhath01 <isuruprabhath111@gmail>
2023-11-25 11:39:12 +0530

    Use DataTypes in TS

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-console.log("hello TypeScript");
-console.log("pakaya");
+function sum(num1, num2) {
+    return num1 + num2;
+}
+function sumString(num1, num2) {
+    return num1 + num2;
+}
+console.log("sum of 10 + 20 : " + sum(10, 20));
+console.log("sum of String 10 + 20 : " + sumString("10", 20));
