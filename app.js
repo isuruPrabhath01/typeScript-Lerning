e94489eef3a4888695de472b4bcce0bb5dba0bad
isuruPrabhath01 <isuruprabhath111@gmail>
2023-11-25 12:58:13 +0530

    Distance Calculator

--- app.js
+++ app.js
@@ -4,3 +4,15 @@ student = { id: 1, name: 'isuru', age: 22, college: 'UoBolton' };
 student = "Isuru";
 var studentObject;
 studentObject = { id: 1, name: 'isuru', age: 22, college: 'UoBolton' };
+//-------------------------------------Distance Calculator--------------------------------------------
+function calcDistance(location1, location2) {
+    var lon1 = location1.lon;
+    var lat1 = location1.lat;
+    var lon2 = location2.lon;
+    var lat2 = location2.lat;
+    return 123456.98234;
+}
+var location1 = { lon: 0.0, lat: 0.0 };
+var location2 = { lon: 0.0, lat: 0.0 };
+var distance = calcDistance(location1, location2);
+console.log("Distance " + distance.toFixed(2));
