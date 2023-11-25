6b5b37801eeaf9e3471262a1053b045e1bdea5b8
isuruPrabhath01 <isuruprabhath111@gmail>
2023-11-25 12:23:57 +0530

    Type guard And multi Type dataTypes

--- tasks/4_DefinedFuntionReturnType.ts
+++ tasks/4_DefinedFuntionReturnType.ts
@@ -3,3 +3,14 @@ function  sendSum(num1 : number, num2 :number, name : string):number{
 }
 
 console.log(sendSum(10,20,"isuru"));
+
+function  sendSum2(num1 : number|string, num2 :number, name : string):number{
+    if (typeof num1 === 'string'){//Type guard
+        return +num1+num2;
+    }else{
+        return num1+num2;
+    }
+}
+
+console.log(sendSum2(10,20,"isuru"));
+console.log(sendSum2("10",20,"isuru"));
