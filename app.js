80e91dbc86b14ddef94bc182eba695fb53e47dd6
isuruPrabhath01 <isuruprabhath111@gmail>
2023-11-25 15:50:56 +0530

    Create Singleton Classes

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
-//Object handling
-var student;
-student = { id: 1, name: 'isuru', age: 22, college: 'UoBolton' };
-student = "Isuru";
-var studentObject;
-studentObject = { id: 1, name: 'isuru', age: 22, college: 'UoBolton' };
-//-------------------------------------Distance Calculator--------------------------------------------
-function calcDistance(location1, location2) {
-    var lon1 = location1.lon;
-    var lat1 = location1.lat;
-    var lon2 = location2.lon;
-    var lat2 = location2.lat;
-    return 123456.98234;
-}
-var location1 = { lon: 0.0, lat: 0.0 };
-var location2 = { lon: 0.0, lat: 0.0 };
-var distance = calcDistance(location1, location2);
-console.log("Distance " + distance.toFixed(2));
+var DBConnection = /** @class */ (function () {
+    function DBConnection(value) {
+        DBConnection.connection = value;
+    }
+    DBConnection.getConnection = function () {
+        if (!DBConnection.connection) {
+            new DBConnection('testConnection');
+        }
+        return DBConnection.connection;
+    };
+    return DBConnection;
+}());
+var connection = DBConnection.getConnection();
+console.log('Connection : ', connection);
--- tasks/8_CreateClassesInTS.ts
+++ tasks/8_CreateClassesInTS.ts
@@ -18,6 +18,29 @@ let person1 = new Person("Isuru", 22, "200123601322");
 console.log(person1);
 person1.name="Prabhath";
 console.log(person1);
-//console.log(person1.age):// cannot access out of class scope
-console.log(person1.getAge()); // access using get method
-//person1.nic="200123601323";  //Cannot update because "readonly" keyword;
+//console.log(person1.age):-------->> cannot access out of class scope
+console.log(person1.getAge());//------>> access using get method
+//person1.nic="200123601323";------>> Cannot update because "readonly" keyword
+
+
+
+
+//-----------Create singleton class----------------
+
+class DBConnection{
+   static connection: string;
+
+    private constructor(value : string) {
+        DBConnection.connection=value;
+    }
+
+    static getConnection(){
+        if (!DBConnection.connection){
+            new DBConnection('testConnection')
+        }
+        return DBConnection.connection;
+    }
+}
+
+let connection:string = DBConnection.getConnection();
+console.log('Connection : ',connection);
