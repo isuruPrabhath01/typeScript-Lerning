18fccf0d187fe21a9276b71a20085ffd4158fb8a
isuruPrabhath01 <isuruprabhath111@gmail>
2023-11-25 15:27:31 +0530

    Create Classes

--- tasks/8_CreateClassesInTS.ts
+++ tasks/8_CreateClassesInTS.ts
@@ -3,7 +3,7 @@
 class Person{
     name : string;
     age : number;
-    nic : string;
+    readonly nic : string;
     constructor(name : string , age : number , nic : string) {
         this.name=name;
         this.age=age;
@@ -13,3 +13,8 @@ class Person{
 
 let person1 = new Person("Isuru", 22, "200123601322");
 console.log(person1);
+person1.name="Prabhath";
+console.log(person1);
+person1.age=21;
+console.log(person1);
+//person1.nic="200123601323";  //Cannot update because "readonly" keyword;
